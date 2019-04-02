3cce9de31ce3c2dcb3b64b2b76da320e4cd9bfed
mimr <mimr@mimr-aspire.fritz.box>
2019-04-02 23:02:48 +0200

    Init repo

---
+++ src/main/java/com/mimr/App.java
@@ -0,0 +1,14 @@
+package com.mimr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+
+@SpringBootApplication
+@PropertySource("classpath:application.properties")
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
